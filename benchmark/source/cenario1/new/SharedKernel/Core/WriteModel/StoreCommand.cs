8b5bef5b8b0df8ad35aa1db24dda26a64c17b384
drr00t <cr1pt0#gmaer>
2020-02-13 17:09:34 -0300

    new types to StoreCommand splited classes in different namespaces

---
+++ benchmark/source/cenario1/new/SharedKernel/Core/WriteModel/StoreCommand.cs
@@ -0,0 +1,17 @@
+﻿namespace SharedKernel.Core
+{
+    public class StoreCommand:IParameter
+    {
+        public StoreCommand(ulong id, string commandType, byte[] commandData)
+        {
+            Id = id;
+            CommandType = commandType;
+            CommandData = commandData;
+        }
+
+        public ulong Id { get;}
+        public string CommandType { get;}
+        public byte[] CommandData { get; }
+        
+    }
+}
---
+++ benchmark/source/cenario1/new/SharedKernel/Core/WriteModel/StoreOperationHandler.cs
@@ -0,0 +1,21 @@
+﻿using System.Threading.Tasks;
+using NetMQ;
+using NetMQ.Sockets;
+
+namespace SharedKernel.Core
+{
+    public abstract class StoreOperationHandler: OperationHandler<StoreCommand>
+    {
+      
+        public StoreOperationHandler(string externalEndPoint, string replicationEndPoint)
+            :base(externalEndPoint,replicationEndPoint)
+        {
+
+        }
+
+        protected override Task ExecuteOperation(StoreCommand command)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
